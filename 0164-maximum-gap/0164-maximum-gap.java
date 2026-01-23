974a790d7184a07f3c5b10b54d61d5759df9841f
Anvesh Anumolu <anvesh.a9100@gmail.com>
2026-01-23 15:06:27 +0530

    Time: 46 ms (37.68%), Space: 90.7 MB (69.58%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length<2) return 0;
+        Arrays.sort(nums);
+        int gap=0;
+        for(int i=0;i<nums.length-1;i++){
+            gap=Math.max(gap,nums[i+1]-nums[i]);
+        }
+        return gap;
+    }
+}
