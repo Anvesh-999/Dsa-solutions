b93d31e054151fb9535fa298015ce7e89c23ce03
Anvesh Anumolu <anvesh.a9100@gmail.com>
2026-09-25 23:28:45 +0530

    Time: 4 ms (36.72%), Space: 43.4 MB (25.75%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,17 +3,17 @@ class Solution {
         Stack<Character> str=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
-                str.push(ch);
-            }else {
-                if(str.isEmpty()) return false;
-               if((str.peek()=='('&&ch==')') || (str.peek()=='{'&&ch=='}') || (str.peek()=='['&&ch==']')){
-                str.pop();
+            if(ch=='('||ch=='{' ||ch=='['){
+                str.push(s.charAt(i));
             }else{
-                return false;
+                if(str.isEmpty()) return false;
+                if((str.peek()=='('&& ch==')')||(str.peek()=='{' && ch=='}')||(str.peek()=='[' && ch==']')){
+                    str.pop();
+                }else{
+                    return false;
+                }
             } 
-            }
         }
-        return str.isEmpty();
+    return str.isEmpty();
     }
 }
